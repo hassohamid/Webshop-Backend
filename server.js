4c01a6686e882e57c7dbb0d9dc3b89085c2618b0
Maxx2774 <maxviggonordin@hotmail.se>
2025-03-20 14:09:06 +0100

    test

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ message: "Success" });
+});
+app.get("/home", (req, res) => {
+  const { username } = req.body;
+  res.json({ message: username });
+});
 const port = 3000;
+
+app.listen(port, () => {
+  console.log(port);
+});
